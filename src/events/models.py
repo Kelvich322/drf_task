faa4a4329ee170df0f935a0301831435d9b144c0
Kelvich Bogdan <kelvichrd@gmail.com>
2025-11-06 15:43:54 -0800

    Add models in models.py.  Make migration. Fix app name in apps.py and urls.py.

--- src/events/models.py
+++ src/events/models.py
@@ -1,3 +1,29 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class EventVenue(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=255, null=False)
+
+    class Meta:
+        db_table = "event_venues"
+
+
+class Event(models.Model):
+    class EventStatus(models.TextChoices):
+        OPEN = "open", "Open"
+        CLOSED = "closed", "Closed"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=255, null=False)
+    date = models.DateTimeField(null=False)
+    status = models.CharField(
+        max_length=10,
+        choices=EventStatus.choices,
+        null=False
+    )
+    venue = models.ForeignKey(EventVenue, on_delete=models.CASCADE, null=True)
+
+    
