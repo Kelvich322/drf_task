70ba41f7e256cf35846d696e27aa90e3c31a2fc6
Kelvich Bogdan <kelvichrd@gmail.com>
2025-11-06 16:07:47 -0800

    Add serializers for events models

---
+++ src/events/serializers.py
@@ -0,0 +1,14 @@
+from models import Event, EventVenue
+from rest_framework import serializers
+
+
+class EventsVenueSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = EventVenue
+        fields = ["id", "name"]
+
+
+class EventsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Event
+        fields = ["id", "name", "date", "status", "venue"]
